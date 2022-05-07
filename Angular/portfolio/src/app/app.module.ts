248742544b57da11ec867898b7e1be3c9332e927
Sebastian Amin Fariña <amiin74@gmail.com>
2022-05-07 13:44:27 -0300

    Nueva Imagen de portfolio

--- Angular/portfolio/src/app/app.module.ts
+++ Angular/portfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { WorksComponent } from './components/works/works.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    MenuComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorksComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
